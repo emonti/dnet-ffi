c88b347db960d00fc8c66d22abe51f59c2a46f6d
Eric Monti <emonti@matasano.com>
2009-09-09 01:06:28 -0500

    added arp ops

--- lib/dnet/constants.rb
+++ lib/dnet/constants.rb
@@ -17,9 +17,16 @@ module Dnet
 
   ARP_HDR_LEN     = 8        # base ARP header length
   ARP_ETHIP_LEN   = 20      # base ARP message length
+
   ARP_HRD_ETH     = 0x0001  # ethernet hardware
   ARP_HRD_IEEE802 = 0x0006  # IEEE 802 hardware
+
   ARP_PRO_IP      = 0x0800  # IP protocol
+
+  ARP_OP_REQUEST    = 1  # request to resolve ha given pa
+  ARP_OP_REPLY      = 2  # response giving hardware address
+  ARP_OP_REVREQUEST = 3  # request to resolve pa given ha
+  ARP_OP_REVREPLY   = 4  # response giving protocol address
 
 
   ######################################################################
