4ff2c65d23b2002388c2fb180511da4b0f576ca5
Eric Monti <emonti@matasano.com>
2009-09-09 18:31:59 -0500

    collected some types

--- lib/dnet/typedefs.rb
+++ lib/dnet/typedefs.rb
@@ -4,6 +4,7 @@ module Dnet
   typedef :uint32,  :ip_addr_t
 
   typedef :pointer, :eth_t
+  typedef :pointer, :fw_t
   typedef :pointer, :intf_t
   typedef :pointer, :ip_t
   typedef :pointer, :route_t
