fdc81fb9fd00044dc0f22385bbbc61d967d9d534
Eric Monti <emonti@matasano.com>
2009-09-12 23:01:48 -0500

    added sockaddr_in specs

--- spec/bsd_spec.rb
+++ spec/bsd_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Dnet::AF" do
   context "address family lookups" do
-    it "should map constants for addres families" do
+    it "should map constants for address families" do
       Dnet::AF.const_get("LINK").should == Socket::AF_LINK
       Dnet::AF.const_get("INET").should == Socket::AF_INET
       Dnet::AF.const_get("INET6").should == Socket::AF_INET6
@@ -26,6 +26,35 @@ describe "Dnet::AF" do
       Dnet::AF[ :inet6 ].should == Socket::AF_INET6
     end
 
-
   end
+end
+
+describe "SockAddrIn" do
+  context "from Socket::pack_sockaddr_in / to unpack_sockaddr_in" do
+    before(:all) do
+      @raw= ::Socket.pack_sockaddr_in(9999, "127.0.0.1")
+      @sa = ::Dnet::SockAddrIn.new :raw => @raw
+    end
+
+    it "should capture a packed sockaddr_in" do
+      @sa.to_ptr.read_string(@sa.size).should == @raw
+    end
+
+    it "should return the correct family value family()" do
+      @sa.family.should == ::Socket::AF_INET
+    end
+
+    it "should return the correct family name with lookup_family()" do
+      @sa.lookup_family.should == "INET"
+    end
+
+    it "should return 8 bytes of null data starting at :_sa_zero" do
+      (@sa.to_ptr + @sa.offset_of(:_sa_zero)).read_string(8).should == "\x00"*8
+    end
+
+    it "should unpack to the correct address" do
+      (::Socket.unpack_sockaddr_in(@sa.to_ptr.read_string(@sa.size))).should == [9999, "127.0.0.1"]
+    end
+  end
+
 end
