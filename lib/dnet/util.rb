e3de426d2e47b62b1bf8621479ea2ee5442631f0
Eric Monti <emonti@matasano.com>
2009-09-13 19:13:30 -0500

    helper funcs for ip string/uint32 conversion

--- lib/dnet/util.rb
+++ lib/dnet/util.rb
@@ -1,54 +1,70 @@
 
 module Dnet
   module Util
-    include FFI::Library
 
-    def self.unhexify(str, d=/\s*/)
-      str.to_s.strip.gsub(/([A-Fa-f0-9]{1,2})#{d}?/) { $1.hex.chr }
-    end
+    # A number of helper methods which can be used to extend class, instance, 
+    # or module
+    module Helpers
+      RX_IP4_ADDR = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
 
-    # Attempts to derive a memroy pointer and length from an "anonymous" object.
-    # Returns an an array object containing [len, pointer]
-    #
-    # buf can be a String or FFI::Pointer. If it is a Pointer, bsz must also
-    # be supplied for the length. If bsz is included with a String, the string
-    # will be truncated if it is longer.
-    #
-    # This is mostly used to support multiple argument types in various 
-    # functions.
-    def self.derive_pointer(buf, bsz=nil)
-      case buf
-      when ::FFI::Pointer
-        raise "no length specified for the pointer" if bsz.nil?
-        raise "size must be a number >= 0" unless bsz.is_a? Numeric and bsz >= 0
-        raise "null pointer #{buf.inspect}" if buf.address == 0
-        pbuf = buf
-      when String
-        buf = buf[0,bsz] if bsz
-        pbuf = ::FFI::MemoryPointer.from_string(buf)
-        bsz = buf.size
-      else
-        raise "cannot derive a pointer and size from a #{buf.class}"
+
+      def unhexify(str, d=/\s*/)
+        str.to_s.strip.gsub(/([A-Fa-f0-9]{1,2})#{d}?/) { $1.hex.chr }
       end
-      return [pbuf, bsz]
-    end
-    
 
-    private
+      # Attempts to derive a memory pointer and length from an "anonymous" object.
+      # Returns an an array object containing [len, pointer]
+      #
+      # buf can be a String or FFI::Pointer. If it is a Pointer, bsz must also
+      # be supplied for the length. If bsz is included with a String, the string
+      # will be truncated if it is longer.
+      #
+      # This is mostly used to support multiple argument types in various 
+      # functions.
+      def derive_pointer(buf, bsz=nil)
+        case buf
+        when ::FFI::Pointer
+          raise "no length specified for pointer" if bsz.nil?
+          raise "size must be a number >= 0" unless bsz.is_a? Numeric and bsz >= 0
+          raise "null pointer #{buf.inspect}" if buf.address == 0
+          pbuf = buf
+        when String
+          buf = buf[0,bsz] if bsz
+          pbuf = ::FFI::MemoryPointer.from_string(buf)
+          bsz = buf.size
+        else
+          raise "cannot derive a pointer and size from a #{buf.class}"
+        end
+        return [pbuf, bsz]
+      end
+      
+      # takes a IPv4 number and returns it as a 32-bit number
+      def ipv4_atol(str)
+        unless str =~ /^#{RX_IP4_ADDR}$/
+          raise(::ArgumentError, "invalid IP address #{str.inspect}")
+        else
+          u32=0
+          str.split('.',4).each {|o| u32 = ((u32 << 8) | o.to_i) }
+          return u32
+        end
+      end
 
-    # alias to class method
-    def derive_mempointer(*args)
-      ::Dnet::Util.derive_mempointer(*args)
-    end
+      # takes a 32-bit number and returns it as an IPv4 address string.
+      def ipv4_ltoa(int)
+        unless(int.is_a? Numeric and int <= 0xffffffff)
+          raise(::ArgumentError, "not a long integer: #{int.inspect}")
+        end
+        ret = []
+        4.times do 
+          ret.unshift(int & 0xff)
+          int >>= 8
+        end
+        ret.join('.')
+      end
 
-    # alias to class method
-    def unhexify(*args)
-      ::Dnet::Util.unhexify(*args)
-    end
-  end # Dnet::Util
+    end # module ClassMethods
 
-  attach_function :htons, [:uint16], :uint16
-  attach_function :ntohs, [:uint16], :uint16
-  attach_function :htonl, [:uint32], :uint32
-  attach_function :ntohl, [:uint32], :uint32
+    extend(Helpers)
+  end # module Util
+
 end
